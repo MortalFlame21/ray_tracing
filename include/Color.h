7c1981061aa73089df505f2bf9b1467d188cd4b8
MortalFlame21 <markgio2100@gmail.com>
2026-03-03 22:25:28 +1100

    Color.h. Add write_color util.

---
+++ include/Color.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <iostream>
+
+#include "Vec3.h"
+
+using Color = Vec3;
+
+void write_color(const Color& pixel_color) {
+    auto r{pixel_color.x()};
+    auto g{pixel_color.y()};
+    auto b{pixel_color.z()};
+
+    auto rbyte{static_cast<int>(255.999 * r)};
+    auto gbyte{static_cast<int>(255.999 * g)};
+    auto bbyte{static_cast<int>(255.999 * b)};
+
+    std::cout << rbyte << ' ' << gbyte << ' ' << bbyte << '\n';
+}
