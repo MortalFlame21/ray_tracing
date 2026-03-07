da9d4088f0cb11335d27cb62be834307a2bf8e69
MortalFlame21 <markgio2100@gmail.com>
2026-03-07 20:08:12 +1100

    main.cpp. Add rays, camera, background.

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,35 @@
 #include <iostream>
 
 #include "Color.h"
+#include "Ray.h"
 #include "Vec3.h"
 
+Color ray_color(const Ray& r) {
+    auto unit_direction{unit_vector(r.direction())};
+    auto a{0.5 * (unit_direction.y() + 1.0)};
+    return (1.0 - a) * Color(1.0, 1.0, 1.0) + a * Color(0.5, 0.7, 1.0);
+}
+
 int main() {
-    int img_w{256};
-    int img_h{256};
+    auto aspect_ratio{16.0 / 9.0};
+    auto img_w{400};
+    auto img_h{static_cast<int>(img_w / aspect_ratio)};
+    img_h = (img_h < 1) ? 1 : img_h;
+
+    auto focal_length{1.0};
+    auto viewport_h{2.0};
+    auto viewport_w{viewport_h * (static_cast<double>(img_w) / img_h)};
+    Vec3 camera_center{};
+
+    Vec3 viewport_u{viewport_w, 0, 0};
+    Vec3 viewport_v{0, -viewport_w, 0};
+
+    auto pixel_delta_u{viewport_u / img_w};
+    auto pixel_delta_v{viewport_v / img_h};
+
+    auto viewport_upper_left{camera_center - Vec3(0, 0, focal_length) - viewport_u / 2 -
+                             viewport_v / 2};
+    auto pixel100_loc{viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v)};
 
     // header
     std::cout << "P3\n" << img_w << ' ' << img_w << "\n255\n";
@@ -13,8 +37,10 @@ int main() {
     for (int j{}; j < img_h; ++j) {
         std::clog << "\rScanlines remaining: " << (img_h - j) << ' ' << std::flush;
         for (int i{}; i < img_w; ++i) {
-            auto pixel_color{Color(static_cast<double>(i) / (img_w - 1),
-                                   static_cast<double>(j) / (img_h - 1), 0.0)};
+            auto pixel_center{pixel100_loc + (i * pixel_delta_u) + (j * pixel_delta_v)};
+            auto ray_direction{pixel_center - camera_center};
+            Ray r{camera_center, ray_direction};
+            auto pixel_color{ray_color(r)};
 
             write_color(pixel_color);
         }
