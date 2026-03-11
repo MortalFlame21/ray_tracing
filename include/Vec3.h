5be8c13e8b2c43578252d5fdf46e2fb2485eff53
MortalFlame21 <markgio2100@gmail.com>
2026-03-12 10:48:19 +1100

    Vec3.h. Update Vec3. New util.

--- include/Vec3.h
+++ include/Vec3.h
@@ -8,6 +8,9 @@ public:
     Vec3();
     Vec3(double x, double y, double z);
 
+    static Vec3 gen_random(int min, int max);
+    static Vec3 gen_random();
+
     double& x();
     double x() const;
     double& y();
@@ -34,6 +37,25 @@ private:
 inline double dot(const Vec3& u, const Vec3& v) {
     return u.x() * v.x() + u.y() * v.y() + u.z() * v.z();
 }
+
 inline Vec3 unit_vector(const Vec3& v) { return v / v.length(); }
+
+inline Vec3 random_unit_vector() {
+    // Rejection method: Reject bad vector samples until we find a good one.
+    while (true) {
+        auto p {Vec3::gen_random(-1, -1)};
+        auto len_sq{p.length_squared()};
+
+        if (1e-160 < len_sq && len_sq <= 1)
+            return p / std::sqrt(len_sq);
+    }
+}
+
+inline Vec3 random_on_hemisphere(const Vec3& normal) {
+    auto on_unit_sphere{random_unit_vector()};
+    // invert the vector (not on hemisphere)
+    return (dot(on_unit_sphere, normal) > 0.0) ? on_unit_sphere : -on_unit_sphere;
+}
+
 std::ostream& operator<<(std::ostream& out, const Vec3& v);
 Vec3 operator*(double t, const Vec3& o);
--- src/Vec3.cpp
+++ src/Vec3.cpp
@@ -1,10 +1,19 @@
 #include <cmath>
 
 #include "Vec3.h"
+#include "Util.h"
 
 Vec3::Vec3() : m_e{{}} {}
 
 Vec3::Vec3(double x, double y, double z) : m_e{{x, y, z}} {}
+
+Vec3 Vec3::gen_random(int min, int max) {
+    return Vec3(gen_random_double(min, max), gen_random_double(min, max), gen_random_double(min, max));
+}
+
+Vec3 Vec3::gen_random() {
+    return Vec3(gen_random_double(), gen_random_double(), gen_random_double());
+}
 
 double& Vec3::x() { return m_e[0]; }
 double Vec3::x() const { return m_e[0]; }
